21228c7d4ebbc931644d8b7e650f9cb4e9d4d6da
PrasadDheb <Prasad.Dheb@crossasyst.com>
2023-01-22 21:46:00 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import { useState } from 'react';
 import { StyleSheet,ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
-
+import GameScreen from './screens/GameScreen';
 export default function App() {
-  const [useNumber,setNumber] =useState();
+  const [userNumber,setUserNumber] =useState();
+
+  function pickedNumberHandler(pickedNumber){
+      setUserNumber(pickedNumber);
+  }
+  let screen =<StartGameScreen/>;
+  if(userNumber){
+    screen=<GameScreen/>
+  }
   return (
         <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
                 <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover' style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-                      <StartGameScreen/>
+                     {screen}
                 </ImageBackground>
         </LinearGradient>
         )
